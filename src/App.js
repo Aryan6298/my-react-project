29fbd4436cdcb432721549989c7f4e7a928e9473
Aryan6298 <ajrjaj123123@gmail.com>
2025-04-21 12:10:24 +0530

    Switched to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -10,14 +9,12 @@ import Contact from "./pages/Contact";
 const App = () => {
   return (
     <Router>
-     
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-  
     </Router>
   );
 };
